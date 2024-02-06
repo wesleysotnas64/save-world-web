dab7735c143d9957a6b67b58c9a499e4e937e7e6
GuiHQz <guilher.queiroz@gmail.com>
2024-02-06 18:18:34 -0300

    feat: Add new main-content

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import { Navbar } from "./components/navbar/navbar"
+import MainFrame from './components/main-frame/main-frame'
 
 function App() {
   
   return (
     <>
       <Navbar />
+      <MainFrame />
     </>
   )
 }
